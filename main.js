e8d08cb24aeb944c766ee4813e2b9f66cc6c36e8
Ryan Pettinato <rnpettin@asu.edu>
2025-04-26 04:47:17 -0700

    update
    update

--- main.js
+++ main.js
@@ -14,6 +14,9 @@
 let svg1;
 let svg2;
 
+let svg1Width;
+let svg1Height;
+
 let data1;
 
 document.addEventListener('DOMContentLoaded', function () {} )
@@ -36,18 +39,50 @@ function barChartDraw()
     //     .attr("cx", 50)
     //     .attr("cy", 50)
     //     .attr("r", 50)
+    let rect1 = svg1.node().getBoundingClientRect();
+    svg1Width = rect1.width;
+    svg1Height = rect1.height;
+    let marginX = 100;
+    let marginY = 50;
 
     // make color scale
     const scaleSoldCopies = d3.scaleOrdinal()
         .domain(["Mario", "Turismo", "Speed"])
         .range(["red", "green", "blue"])
 
+    var xAxis = d3.scaleBand()
+        .range([marginX, svg1Width-marginX])
+        .domain(data1[0].map(function(x) { return x.Title}))
+
+    var xAxisDraw = svg1.append("g")
+        .call(d3.axisBottom(xAxis))
+        .attr("transform", "translate(0, 350)")
+        .selectAll("text")            
+            .data(data1[0])
+            .attr("transform", "translate(-10,10)rotate(-55)")
+            .style("text-anchor", "end")
+            .style("font-size", "18px")
+            .style("font-family", "Arial")
+            .style("font-weight", "bold")
+            .style("fill", function(d) { return scaleSoldCopies(d.Type) })
+            .attr("class", "xlabels")
+
+    var yAxis = d3.scaleLinear()
+        .range([svg1Height-marginY, marginY])
+        .domain([0, 70])
+    
+    var yAxisDraw = svg1.append("g")
+        .call(d3.axisRight(yAxis))
+        .attr("transform", "translate(900, -200)")
+
+    console.log(xAxisDraw.selectAll("text"))
+    
     // make x axis scale
 
     // make y axis scale
 
     console.log(data1[0])
-    svg1.selectAll("bars")
+    svg1.selectAll("mybars")
         .data(data1[0])
         .join(
             enter => enter.append("rect")
